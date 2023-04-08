f026533bb718e787c06697563918ec68317c1221
turecross321 <turesod@gmail.com>
2023-04-08 23:39:36 +0200

    Removed unused stuff from GameAlbum.cs

--- SoundShapesServer/Types/Albums/GameAlbum.cs
+++ SoundShapesServer/Types/Albums/GameAlbum.cs
@@ -9,8 +9,6 @@ public class GameAlbum : RealmObject
     public string id { get; set; }
     public string name { get; set; }
     public string artist { get; set; }
-    public string thumbnailURL { get; set; }
-    public string sidePanelURL { get; set; }
     public IList<LinerNote> linerNotes { get; }
     public DateTimeOffset date { get; set; }
     public IList<GameLevel> levels { get; }
