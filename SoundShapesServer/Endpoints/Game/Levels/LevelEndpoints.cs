81f8fd550af0564548ff13d6da51383b1d688609
turecross321 <turesod@gmail.com>
2023-05-30 15:47:10 +0200

    Made so you can only see other peoples liked levels in game

--- SoundShapesServer/Endpoints/Game/Levels/LevelEndpoints.cs
+++ SoundShapesServer/Endpoints/Game/Levels/LevelEndpoints.cs
@@ -78,7 +78,6 @@ public class LevelEndpoints : EndpointGroup
     }
     
     [GameEndpoint("~identity:{userId}/~queued:*.page", ContentType.Json)]
-    [GameEndpoint("~identity:{userId}/~like:*.page", ContentType.Json)]
     public RelationLevelsWrapper? QueuedAndLiked(RequestContext context, GameDatabaseContext database, GameUser user, string userId)
     {
         int count = int.Parse(context.QueryString["count"] ?? "9");
@@ -88,6 +87,20 @@ public class LevelEndpoints : EndpointGroup
         if (userToGetLevelsFrom == null) return null;
         
         (GameLevel[] levels, int totalLevels) = database.GetLevels(LevelOrderType.DoNotOrder, true, new LevelFilters(likedOrQueuedByUser: userToGetLevelsFrom), from, count);
+        
+        return new RelationLevelsWrapper(levels, user, totalLevels, from, count);
+    }
+    
+    [GameEndpoint("~identity:{userId}/~like:*.page", ContentType.Json)]
+    public RelationLevelsWrapper? Liked(RequestContext context, GameDatabaseContext database, GameUser user, string userId)
+    {
+        int count = int.Parse(context.QueryString["count"] ?? "9");
+        int from = int.Parse(context.QueryString["from"] ?? "0");
+        
+        GameUser? userToGetLevelsFrom = database.GetUserWithId(userId);
+        if (userToGetLevelsFrom == null) return null;
+        
+        (GameLevel[] levels, int totalLevels) = database.GetLevels(LevelOrderType.DoNotOrder, true, new LevelFilters(likedByUser: userToGetLevelsFrom), from, count);
         
         return new RelationLevelsWrapper(levels, user, totalLevels, from, count);
     }
