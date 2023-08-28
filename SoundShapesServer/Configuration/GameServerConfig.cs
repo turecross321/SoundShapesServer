8597bb9c1b9c9e64149ffe784c7e852b229f4628
turecross321 <51852312+turecross321@users.noreply.github.com>
2023-08-28 19:03:21 +0200

    Upgrade Bunkum

--- SoundShapesServer/Configuration/GameServerConfig.cs
+++ SoundShapesServer/Configuration/GameServerConfig.cs
@@ -5,7 +5,7 @@ namespace SoundShapesServer.Configuration;
 
 public class GameServerConfig : Config
 {
-    public override int CurrentConfigVersion => 3;
+    public override int CurrentConfigVersion => 4;
     public override int Version { get; set; }
     
     protected override void Migrate(int oldVer, dynamic oldConfig)
@@ -13,6 +13,11 @@ public class GameServerConfig : Config
         if (CurrentConfigVersion < 2)
         {
             RequireAuthentication = (bool)oldConfig.ApiAuthentication;
+        }
+
+        if (CurrentConfigVersion < 4)
+        {
+            RateLimitSettings.Bucket = RateLimitSettings.DefaultBucket;
         }
     }
     
@@ -26,5 +31,5 @@ public class GameServerConfig : Config
     public string EmailHost { get; set; } = "smtp.gmail.com";
     public int EmailHostPort { get; set; } = 587;
     public bool EmailSsl { get; set; } = true;
-    public RateLimitSettings RateLimitSettings = new (30, 400, 0);
+    public RateLimitSettings RateLimitSettings = new (30, 400, 0, RateLimitSettings.DefaultBucket);
 }
