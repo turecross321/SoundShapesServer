40af453f0f0ef467537edd7ec13db88976ef7610
turecross321 <51852312+turecross321@users.noreply.github.com>
2024-09-21 19:41:43 +0200

    Add VerifiedEmail & FinishedRegistration to ApiFullUserResponse.cs

--- SoundShapesServer/Types/Responses/Api/DataTypes/ApiFullUserResponse.cs
+++ SoundShapesServer/Types/Responses/Api/DataTypes/ApiFullUserResponse.cs
@@ -8,6 +8,8 @@ public record ApiFullUserResponse : IApiDbResponse<DbUser, ApiFullUserResponse>
     public required Guid Id { get; set; }
     public required string Name { get; set; }
     public required UserRole Role { get; set; }
+    public required bool VerifiedEmail { get; set; }
+    public required bool FinishedRegistration { get; set; }
     
     public static ApiFullUserResponse FromDb(DbUser value)
     {
@@ -15,7 +17,9 @@ public record ApiFullUserResponse : IApiDbResponse<DbUser, ApiFullUserResponse>
         {
             Id = value.Id,
             Name = value.Name,
-            Role = value.Role
+            Role = value.Role,
+            VerifiedEmail = value.VerifiedEmail,
+            FinishedRegistration = value.FinishedRegistration
         };
     }
 }
