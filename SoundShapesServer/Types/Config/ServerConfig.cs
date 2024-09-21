9c1f71fdcf0df8d82243241e0dcc4e6ec626f7c2
turecross321 <51852312+turecross321@users.noreply.github.com>
2024-09-21 23:49:09 +0200

    Change default repository URL

--- SoundShapesServer/Types/Config/ServerConfig.cs
+++ SoundShapesServer/Types/Config/ServerConfig.cs
@@ -8,7 +8,7 @@ public class ServerConfig: Bunkum.Core.Configuration.Config
     public string PostgresSqlConnectionString { get; set; } = "Host=localhost;Username=username;Password=password;Database=soundshapes";
     public bool RequireAuthentication { get; set; }
     public string EulaText { get; set; } = "";
-    public string RepositoryUrl { get; set; } = "https://github.com/turecross321/SoundShapesServer.git";
+    public string RepositoryUrl { get; set; } = "https://github.com/turecross321/SoundShapesServer";
     public string WebsiteUrl { get; set; } = "https://sound.ture.fish";
     public int LevelPublishLimit = 200;
     public int MaxUgcBytesPerUser { get; set; }= 128_000_000;
