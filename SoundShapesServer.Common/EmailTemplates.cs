a26ddf56000f9434cc88e30d40da43b635fd8d89
turecross321 <51852312+turecross321@users.noreply.github.com>
2024-09-23 22:57:19 +0200

    Wrong way around

--- SoundShapesServer.Common/EmailTemplates.cs
+++ SoundShapesServer.Common/EmailTemplates.cs
@@ -6,27 +6,29 @@ public static class EmailTemplates
   {
     string verifyUrl = $"{websiteUrl}/verifyEmail?code={verifyEmailCode}";
 
-    string html = hasUserFinishedRegistration ? """
-                                             <html lang="en" style="font-size: 10pt; font-family: Tahoma, serif;">
-                                               <body style="color:#181515;">
-                                                <h1>Hello, {USER}</h1>
-                                                <p>Please click the button below to verify your email address and finish the registration of your account.</p>
-                                                <a href="{CODE_URL}" style="color: white; background-color: #F07167; padding: 0.5rem 1rem; border-radius: 0.5rem; font-size: x-large; text-decoration: none; display: inline-block;">Verify</a>
-                                                <p>If you didn't request this, please ignore this email.</p>
-                                                <p>Greetings, the {INSTANCE} team.</p>
-                                               </body>
-                                             </html>
-                                             """ : """
-                                                   <html lang="en" style="font-size: 10pt; font-family: Tahoma, serif;">
-                                                    <body style="color:#181515;">
-                                                      <h1>Hello, {USER}</h1>
-                                                      <p>Please click the button below to verify your email address.</p>
-                                                      <a href="{CODE_URL}" style="color: white; background-color: #F07167; padding: 0.5rem 1rem; border-radius: 0.5rem; font-size: x-large; text-decoration: none; display: inline-block;">Verify</a>
-                                                      <p>If you didn't request this, please ignore this email.</p>
-                                                      <p>Greetings, the {INSTANCE} team.</p>
-                                                     </body>
-                                                   </html>
-                                                   """;
+    string html = hasUserFinishedRegistration
+      ? """
+        <html lang="en" style="font-size: 10pt; font-family: Tahoma, serif;">
+         <body style="color:#181515;">
+           <h1>Hello, {USER}</h1>
+           <p>Please click the button below to verify your email address.</p>
+           <a href="{CODE_URL}" style="color: white; background-color: #F07167; padding: 0.5rem 1rem; border-radius: 0.5rem; font-size: x-large; text-decoration: none; display: inline-block;">Verify</a>
+           <p>If you didn't request this, please ignore this email.</p>
+           <p>Greetings, the {INSTANCE} team.</p>
+          </body>
+        </html>
+        """
+      : """
+        <html lang="en" style="font-size: 10pt; font-family: Tahoma, serif;">
+          <body style="color:#181515;">
+           <h1>Hello, {USER}</h1>
+           <p>Please click the button below to verify your email address and finish the registration of your account.</p>
+           <a href="{CODE_URL}" style="color: white; background-color: #F07167; padding: 0.5rem 1rem; border-radius: 0.5rem; font-size: x-large; text-decoration: none; display: inline-block;">Verify</a>
+           <p>If you didn't request this, please ignore this email.</p>
+           <p>Greetings, the {INSTANCE} team.</p>
+          </body>
+        </html>
+        """; 
     
     return html
       .Replace("{USER}", userName)
