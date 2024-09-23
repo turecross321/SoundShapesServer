f8ba4a214905b8caef537033d69a8d5780e7483e
turecross321 <51852312+turecross321@users.noreply.github.com>
2024-09-23 19:24:21 +0200

    Database migration

--- SoundShapesServer/Migrations/20240923172354_RegistrationExpiryDate.Designer.cs
+++ SoundShapesServer/Migrations/20240923172354_RegistrationExpiryDate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SoundShapesServer.Database;
@@ -11,9 +12,11 @@ using SoundShapesServer.Database;
 namespace SoundShapesServer.Migrations
 {
     [DbContext(typeof(GameDatabaseContext))]
-    partial class GameDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240923172354_RegistrationExpiryDate")]
+    partial class RegistrationExpiryDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SoundShapesServer/Migrations/20240923172354_RegistrationExpiryDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoundShapesServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegistrationExpiryDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegistrationExpiryDate",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationExpiryDate",
+                table: "Users");
+        }
+    }
+}
