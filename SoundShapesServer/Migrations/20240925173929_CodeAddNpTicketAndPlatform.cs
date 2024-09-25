46da3d31f73cac764ff1c2be2667a698fc6a1c24
turecross321 <51852312+turecross321@users.noreply.github.com>
2024-09-25 19:39:41 +0200

    Code migrations

---
+++ SoundShapesServer/Migrations/20240925173929_CodeAddNpTicketAndPlatform.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoundShapesServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CodeAddNpTicketAndPlatform : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "GenuineNpTicket",
+                table: "Codes",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Platform",
+                table: "Codes",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GenuineNpTicket",
+                table: "Codes");
+
+            migrationBuilder.DropColumn(
+                name: "Platform",
+                table: "Codes");
+        }
+    }
+}
